eb1f5eb059c72939d4b8df85fa995e7c348ddfb0
Dokja123 <danielmalan888@gmail.com>
2024-11-25 20:05:50 +0000

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = ""
-const BOT_NUMBER = ""
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "2250100113543"
+const SESSION_ID = "PRABATH-MD~9vkEFRzT#_QV2VvEvHn5tTe7OQK89pDfzza6Ahlqmq8Xm_8D_X94"
 
 
 module.exports = {
